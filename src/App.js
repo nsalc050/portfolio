1261c9deaba2c4a441227839f7c8db48116d51e3
Nicolas Salcedo <nssalnic@gmail.com>
2021-09-09 15:12:37 -0400

    deploy ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from './js/three.module.js';
 
 function App() {
   return (
@@ -7,16 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>
+            
+
+            
+            </code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <canvas id='bg'>
+
+        </canvas>
       </header>
     </div>
   );
