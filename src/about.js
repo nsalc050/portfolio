f6ea14003ea502d1a1e1a41edd47f1b0c59b8859
Nicolas Salcedo <nssalnic@gmail.com>
2025-10-15 20:47:32 -0400

    updated app added about page

---
+++ src/about.js
@@ -0,0 +1,26 @@
+import logo from './logo.png';
+import './App.css';
+import { useNavigate } from 'react-router-dom';
+function About() {
+    const navigateHome = useNavigate();
+    return(
+        <div className='page1' style={{textAlign: 'center'}}>
+            
+             <header className="App-header">
+
+             <h1>About me</h1>      <img src={logo} alt="logo" size="20px"/>
+             <div className='divider' />
+                
+        
+            </header>
+            <p>Hello, I'm <span className='text-blue-500'>Nicolas Salcedo</span>, a passionate QA Automation Engineer with expertise in creating robust test frameworks and ensuring software quality.</p>
+            <p>I specialize in using tools like Selenium, Playwright, and Cypress to automate web application testing, and I have experience with CI/CD pipelines to integrate testing into the development process.</p>
+            <p>In addition to my technical skills, I am a strong advocate for Agile methodologies and continuous improvement in software development practices.</p> 
+            <p>using react and playwright to test this and another web application</p>
+
+            <button clasname="home-button" onClick={() => navigateHome('/')}> Go Back Home </button>
+        </div>
+    )
+}
+
+export default About;
