fc56b8ac81ed43c04b2bb6d90e5c7cfd117a5ca9
Nicolas Salcedo <nssalnic@gmail.com>
2026-03-12 16:21:49 -0400

    new spec file for test case financial transactions

---
+++ tests/financial_trans_test.spec.ts
@@ -0,0 +1,47 @@
+import {test,expect} from 'playwright/test';
+
+//Samsle transaction data
+
+ const amountValues = [10.00, 1000000.00, 0]
+
+//purchases
+test('process purchase',async ({request}) =>{
+
+    
+
+});
+
+//refund
+test('process refund',async ({request}) =>{
+
+});
+
+//pre_auths
+test('process preauthorization',async ({request}) =>{
+
+});
+
+//pre_auth correction
+test('process preauthorization completion',async ({request}) =>{
+
+});
+
+//purchase correction
+test('process purchase correction',async ({request}) =>{
+
+});
+
+//refund corrections
+test('process refund correction',async ({request}) =>{
+
+});
+//purchase reversal
+//refund corrections
+test('process refund correction',async ({request}) =>{
+
+});
+//pre-auth reversals
+//refund corrections
+test('process refund correction',async ({request}) =>{
+
+});
--- tests/pokedex_data_validation.spec.ts
+++ tests/pokedex_data_validation.spec.ts
@@ -4,14 +4,41 @@ import {test,expect} from 'playwright/test';
 const source = 'https://pokeapi.co/api/v2/pokemon/'
 
 
-test('Pokedex Validation API', async ({request}) =>{
+test('Pokedex Validation API - starter Pokemon - Kanto', async ({request}) =>{
    //
- const pokemon_data = await request.get(source+'arbok');
+ const bulbasaur_data = await request.get(source+'bulbasaur');
  
- expect(pokemon_data.status()).toBe(200);
+ expect(bulbasaur_data.status()).toBe(200);
 
- const body = await pokemon_data.json();
-  console.log(body);   
+ const bulbasaur_dataJSON = await bulbasaur_data.json();
 
- expect (body).toHaveProperty('abilities.ability','overgrow');
+
+ console.log (bulbasaur_dataJSON)
+
+ console.log('Pokemon moves')
+ console.log (bulbasaur_dataJSON.moves);
+// validate that it can learn tackle
+
+ expect (bulbasaur_dataJSON.moves[0]).toHaveProperty('move.name','razor-wind');
+  console.log (bulbasaur_dataJSON.moves[0]);
+
+
+  const charmander_data = await request.get(source+'charmander');
+ 
+ expect(charmander_data.status()).toBe(200);
+
+ const charmander_dataJSON = await charmander_data.json();
+
+
+ console.log (charmander_dataJSON)
+
+ console.log('Pokemon moves')
+ console.log (charmander_dataJSON.moves);
+// validate that it can learn tackle
+
+ expect (charmander_dataJSON.moves[0],"move listed under pokemon").toHaveProperty('move.name','tackle');
+  console.log (charmander_dataJSON.moves[0]);
+
+
+
 });
