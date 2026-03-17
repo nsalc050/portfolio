473c628371162fc4ddfcb5e997dc9a39c9ccf97a
Nicolas Salcedo <nssalnic@gmail.com>
2026-03-17 18:26:18 -0400

    updated transaction tests

--- tests/financial_trans_test.spec.ts
+++ tests/financial_trans_test.spec.ts
@@ -7,43 +7,43 @@ import {test,expect} from 'playwright/test';
 //purchases
 test('process purchase',async ({request}) =>{
 
-    console.log("purchase")
+    console.log("purchase");
 
 });
 
 //refund
 test('process refund',async ({request}) =>{
- console.log("refund")
+ console.log("refund");
 });
 
 //pre_auths
 test('process preauthorization',async ({request}) =>{
- console.log("purchase")
+ console.log("purchase");
 });
 
 //pre_auth correction
 test('process preauthorization completion $0',async ({request}) =>{
- console.log("preauthorization 0 ")
+ console.log("preauthorization 0 ");
 });
 
 test('process preauthorization completion $10.00',async ({request}) =>{
- console.log("purchase  10")
+ console.log("purchase  10");
 });
 
 test('process preauthorization completion $100.00',async ({request}) =>{
- console.log("purchase 100")
+ console.log("purchase 100");
 });
 
 //purchase correction
 test('process purchase correction',async ({request}) =>{
- console.log("purchase correction")
+ console.log("purchase correction");
 });
 
 //refund corrections
 test('process refund correction',async ({request}) =>{
- console.log("refund correction")
+ console.log("refund correction");
 });
 //purchase reversal
 test('process purchase reversal',async ({request}) =>{
- console.log("purchase reversal")
+ console.log("purchase reversal");
 });
